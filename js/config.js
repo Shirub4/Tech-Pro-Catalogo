66a8feca92e98a6498240ab5e29e96570ee43e30
Shirub4 <lucian.gm205@gmail.com>
2026-07-23 00:40:16 -0300

    Delete js directory

--- js/config.js
+++
@@ -1,5 +0,0 @@
-// Configuração pública do Supabase para o catálogo.
-// IMPORTANTE: mantenha os valores entre aspas.
-// A URL deve terminar em .supabase.co, sem /rest/v1/.
-export const SUPABASE_URL = "https://zpnkdhzroigifstufltr.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_BL9BhjpCxTuzGv5MCVLFDw_Kh9cHo2w";
